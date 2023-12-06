ed3034edc728cf291c11d11088ade8c62ea619e2
Kevin Lusignolo <Kevin.Lusignolo@hcss.com>
2023-12-06 09:40:37 -0600

    Minify part 2

--- day 6 - races/part_two.py
+++ day 6 - races/part_two.py
@@ -1,11 +1,10 @@
+def get_the_answer(race_time: int, race_distance: int):
+    for i in range(race_time):
+        if i * (race_time - i) > race_distance:
+            return race_time - i - i + 1
+        
 with open("races.txt", "r") as file:
     time_and_distance = file.read().splitlines()
     race_time = int("".join(time_and_distance[0].split()[1:]))
     race_distance = int("".join(time_and_distance[1].split()[1:]))
-
-for i in range(race_time):
-    if i * (race_time - i) > race_distance:
-        ways_to_win = race_time - i - i + 1
-        break
-print(ways_to_win)
-    
+print(get_the_answer(race_time, race_distance))
