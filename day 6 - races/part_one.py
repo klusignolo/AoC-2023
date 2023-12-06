93d7080c2586e3910dba34b82f3741dce3b06b29
Pegasus\kevin <kevinlusignolo@gmail.com>
2023-12-06 07:55:53 -0600

    Minify answers

--- day 6 - races/part_one.py
+++ day 6 - races/part_one.py
@@ -5,18 +5,11 @@ with open("races.txt", "r") as file:
     race_times = [int(i) for i in time_and_distance[0].split()[1:]]
     race_distances = [int(i) for i in time_and_distance[1].split()[1:]]
 
-def get_distance_traveled(hold_ms: int, race_time: int):
-    travel_time = race_time - hold_ms
-    return travel_time * hold_ms
-
 result = 1
 for i in range(len(race_times)):
-    ways_to_win = 0
-    race_time = race_times[i]
-    distance_to_beat = race_distances[i]
-    for j in range(race_time):
-        if get_distance_traveled(j, race_time) > distance_to_beat:
-            ways_to_win += 1
-    result *= ways_to_win
+    for j in range(race_times[i]):
+        if j * (race_times[i] - j) > race_distances[i]:
+            result *= race_times[i] - j - j + 1
+            break
 print(result)
     
--- day 6 - races/part_two.py
+++ day 6 - races/part_two.py
@@ -3,13 +3,8 @@ with open("races.txt", "r") as file:
     race_time = int("".join(time_and_distance[0].split()[1:]))
     race_distance = int("".join(time_and_distance[1].split()[1:]))
 
-def get_distance_traveled(hold_ms: int, race_time: int):
-    travel_time = race_time - hold_ms
-    return travel_time * hold_ms
-
-ways_to_win = 0
 for i in range(race_time):
-    if get_distance_traveled(i, race_time) > race_distance:
+    if i * (race_time - i) > race_distance:
         ways_to_win = race_time - i - i + 1
         break
 print(ways_to_win)
