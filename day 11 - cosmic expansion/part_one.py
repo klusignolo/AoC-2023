fdc459fc3d6ee7f7be0bb9cfdc3bc89f1f24dc1b
Pegasus\kevin <kevinlusignolo@gmail.com>
2023-12-11 08:29:53 -0600

    Part one in progress

---
+++ day 11 - cosmic expansion/part_one.py
@@ -0,0 +1,34 @@
+with open("test.txt", "r") as file:
+    universe = [[i for i in line] for line in file.read().splitlines()]
+
+def expand(input: list[list[str]], direction: str):
+    indices_to_expand = []
+    if direction == "ns":
+        for i in range(len(input)):
+            if len(set(input[i])) == 1:
+                indices_to_expand.append(i)
+    elif direction == "ew":
+        cols = [set() for _ in range(len(input))]
+        for i in range(len(input)):
+            for j in range(len(input[i])):
+                cols[j].add(input[i][j])
+        for i in range(len(cols)):
+            if len(cols[i]) == 1:
+                indices_to_expand.append(i)
+    width = len(input[0])
+    expanded_so_far = 0
+    for i in range(len(indices_to_expand)):
+        if direction == "ns":
+            input.insert(indices_to_expand[i] + expanded_so_far, ["." for _ in range(width)])
+            expanded_so_far += 1
+        elif direction == "ew":
+            for i in range(len(indices_to_expand)):
+                expanded_so_far = 0
+                for row in input:
+                    row.insert(indices_to_expand[i] + expanded_so_far, ".")
+                expanded_so_far += 1
+expand(universe, "ns")
+expand(universe, "ew")
+for row in universe:
+    print(row)
+print(universe)
