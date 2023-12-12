cc8e2de8a26a0a5a9da9b91e5055a95545edee0f
Pegasus\kevin <kevinlusignolo@gmail.com>
2023-12-11 23:48:46 -0600

    more wip

--- day 12/part_one.py
+++ day 12/part_one.py
@@ -2,11 +2,12 @@ with open("test.txt", "r") as file:
     records = file.read().splitlines()
 for line in records:
     record = line.split()[0]
-    b_list = [[int(j) for j in i] for i in line.split()[1].split(",")]
+    b_list = [int(i) for i in line.split()[1].split(",")]
+    w_remain = len(record) - sum(b_list) - len([i for i in record if i == "."])
     sections = [section for section in record.split(".") if len(section) > 0]
     current_b = 0
     for section in sections:
         b_copy = b_list.copy()
-        if len(section) > b_counts[current_b] + 1:
-            
+        if len(section) > b_list[current_b] + 1:
+            pass
     print(sections)
