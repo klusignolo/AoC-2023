deaa2ff7396167580c00815131c847e8bcc23c80
Pegasus\kevin <kevinlusignolo@gmail.com>
2023-12-12 20:56:43 -0600

    wip. The test pattern works for part 2, but some other issue needs fixing.

--- day 12/part_two.py
+++ day 12/part_two.py
@@ -1,4 +1,4 @@
-with open("test.txt", "r") as file:
+with open("records.txt", "r") as file:
     records = file.read().splitlines()
 
 def does_record_pass(record: str, hash_counts: list[int]) -> bool:
@@ -24,9 +24,7 @@ def does_record_pass(record: str, hash_counts: list[int]) -> bool:
         curr_broke = None
     return len(b_list) == 0 and hash_cnt == 0
 
-possibilities = 0
-r_num = 0
-for line in records:
+def get_possibilities_for_line(line: str):
     record = line.split()[0]
     b_list = [int(i) for i in line.split()[1].split(",")]
     p_count = len(record) - sum(b_list) - len([i for i in record if i == "."])
@@ -43,6 +41,7 @@ for line in records:
         is_valid = i_copy == 0 and p_needed == 0
         if is_valid:
             valid_combos.append(i)
+    possibilities = 0
     if len(valid_combos) == 0:
         possibilities += 1
     for valid_combo in valid_combos:
@@ -54,4 +53,23 @@ for line in records:
         record_to_try = record_to_try.replace("?", "#")
         if does_record_pass(record_to_try, b_list):
             possibilities += 1
+    return possibilities
+possibilities = 0
+r_num = 0
+for line in records:
+    r_num += 1
+    print(f"Calculating line {r_num}")
+    valid_combos_found = get_possibilities_for_line(line)
+    print(f"Calculating extended line {r_num}")
+    if line.split()[0].endswith("#"):
+        extended_combos = valid_combos_found
+    elif line.split()[0].endswith("?"):
+        new_line = f"{line.split()[0]}? {line.split()[1]}"
+        extended_combos = get_possibilities_for_line(new_line)
+    else:
+        extended_combos = get_possibilities_for_line("?" + line)
+    for _ in range(4):
+        valid_combos_found *= extended_combos
+    print(f"Line {r_num} yielded {valid_combos_found} combos!")
+    possibilities += valid_combos_found
 print(possibilities)
